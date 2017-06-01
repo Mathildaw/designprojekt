f35a1c789a6d3bddf76b22640164ca4d2bb3e603
mathildaw <m.athildaa@hotmail.com>
2017-06-01 14:56:06 +0200

    Lagt in referenser

--- design/design/Scripts/Script.js
+++ design/design/Scripts/Script.js
@@ -285,13 +285,11 @@ function hideshown2() {
     z.style.backgroundColor = 'white';
 
 }
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
+
 function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
 
